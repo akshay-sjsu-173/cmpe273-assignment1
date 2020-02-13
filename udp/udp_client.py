b3a9a122da5b4f32d363888d651738bffdcd41c1
Akshay Orpe <akshay.orpe@sjsu.edu>
2020-02-12 17:30:41 -0800

    Added code comments

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -1,16 +1,23 @@
+'''
+This script requires the module func_timeout.
+
+Run the following command to install func_timeout :-
+  
+  pip3 install func_timeout -t /usr/lib/python3.8/
+
+'''
+
 import socket
-import time
 from func_timeout import func_timeout
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
-MESSAGE = "ping"
 
 def getNewConnection():
     return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def send(s=None):
+def sendMessage(s=None):
     wait_time = 3
     try:
         if not s:
@@ -18,27 +25,25 @@ def send(s=None):
         server_ack = True
         with open('upload.txt','r') as currFile:
             message = currFile.readline() #Read first line in file 
-            message_state = [message,5]
-            #retry = 5
-            while message: #and retry>0:
+            message_state = [message,5] #(message, re-connect count)
+            while message:
                 if message != message_state[0]:
-                    message_state = [message,5]
+                    message_state = [message,5] #Reset re-connect count for a new message
                 print("You : ",message.strip())
                 s.sendto(str(message).encode(), (UDP_IP, UDP_PORT))
                 try:
                     data, ip = func_timeout(wait_time,s.recvfrom,(BUFFER_SIZE,))
                     print(data.decode().strip())
                     message = currFile.readline() #Read next line if server ack received
-                    #time.sleep(1)
                 except:
                     print("Server did not respond in ",wait_time," secs. Resending package --> ", message_state)
-                    message_state[1] -= 1
+                    message_state[1] -= 1  #Decrement number of re-connects remaining
                     if message_state[1]>0:
-                        s = getNewConnection()
+                        s = getNewConnection() #Since UDP is stateless, we must re-connect to the server
                     else:
                         exit()
     except socket.error:
         print("Error -->",socket.error)
         exit()
 
-send()
+sendMessage()
